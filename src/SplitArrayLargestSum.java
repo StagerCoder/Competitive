c6a712747b970b3a7ea560a342c9799082b5111b
Rohan <rohankakkar1997@gmail.com>
2023-06-22 18:32:21 +0530

    working in SplitArrayLargestSum

---
+++ src/SplitArrayLargestSum.java
@@ -0,0 +1,11 @@
+
+public class SplitArrayLargestSum {
+    public static void main(String[] args) {
+        int[] arr = {7, 2, 5, 10, 8};
+        int[] arr2 = {1, 2, 3, 4, 5};
+    }
+
+    static int splitArray(int[] nums, int k) {
+
+    }
+}
